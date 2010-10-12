4793fc012bd277cf5979d84f6998aeef7e8ae525
Bradley Meck <bradleymeck@hamburger.local>
2010-10-12 11:26:57 -0500

    cache error fix

--- example/static/sws.js
+++ example/static/sws.js
@@ -1,19 +1,24 @@
 function SessionWebSocket(cb) {
 	var xhr = new XMLHttpRequest()
 	//use https and go over the same port as the server
-	xhr.open("GET","/")
+	xhr.open("GET","/"+(new Date()+0))
 
 	//set our header to get the token
 	xhr.setRequestHeader("x-access-request-token","simple")
 	xhr.onreadystatechange = function xhrverify() {
 		if(xhr.readyState === 4) {
 			var tmp
-			if(tmp = JSON.parse(xhr.responseText)["x-access-token"]) {
-				var socket = new io.Socket();
-				cb(socket)
-				socket.connect()
-				//get the first part as the secret
-				socket.send(tmp.split(";")[0])
+			try {
+				if(tmp = JSON.parse(xhr.responseText)["x-access-token"]) {
+					var socket = new io.Socket();
+					cb(socket)
+					socket.connect()
+					//get the first part as the secret
+					socket.send(tmp.split(";")[0])
+				}
+			}
+			catch(e) {
+				throw new Error("XMLHttpResponse had non-json response, possible cache issue?")
 			}
 		}
 	}
